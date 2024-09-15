fd7f264b1cd351610dbbc7bae0138db25e113036
Nimira43 <n.rabone72@aol.com>
2024-09-15 09:29:46 +0100

    Corrected hr error

--- movie-go-front/src/App.js
+++ movie-go-front/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <div className='col text-end'>
           <a href='#!'><span className='badge bg-danger'>Login</span></a>
         </div>
-        <hr className='col-md-3'/>
+        <hr className='mb-3'/>
       </div>
       <div className="row">
         <div className="col-md-2">
