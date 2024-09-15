17a1815368bc36ac60c0556f46234610706747ef
Nimira43 <n.rabone72@aol.com>
2024-09-15 13:08:30 +0100

    Movie component now able to bring in information of a movie - though only hard coded info about one movie

--- movie-go-front/src/components/Movie.js
+++ movie-go-front/src/components/Movie.js
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+
 const Movie = () => {
+
+  const [movie, setMovie] = useState({})
+  let { id } = useParams()
+
+  useEffect(() => {
+    let myMovie = {
+      id: 1,
+      title: "Star Wars",
+      release_date: "1977-05-25",
+      runtime: 121,
+      mpaa_rating: "PG",
+      description: "A long time ago, in a galaxy far far away...",
+    }
+    setMovie(myMovie)
+  }, [id])
 
   return (
     <div>
-      <h2>Movie</h2>
-      <hr/>
+      <h2>Movie: {movie.title}</h2>
+      <small><em>{movie.release_date}, {movie.runtime} minutes, Rated: {movie.mpaa_rating}</em></small>
+      <hr />
+      <p>{movie.description}</p>
     </div>
   )
 }
