00e7590f5b24b152ac616f152022da725a4f6697
Nimira43 <n.rabone72@aol.com>
2024-09-15 10:26:21 +0100

    Fixed another bug

--- movie-go-front/src/index.js
+++ movie-go-front/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
-const router = createBrowserRouter({
-  path: '/',
-  element: <App />
-})
+import App from './App'
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
