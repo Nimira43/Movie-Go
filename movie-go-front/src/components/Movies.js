6b83c1f8b1c834fe0bfa26ef937c2655c506f282
Nimira43 <n.rabone72@aol.com>
2024-09-15 12:33:00 +0100

    Brought in some dummy database to test the app as it stands

--- movie-go-front/src/components/Movies.js
+++ movie-go-front/src/components/Movies.js
@@ -1,9 +1,60 @@
+import { useEffect, useState } from 'react'
+
 const Movies = () => {
+
+  const { movies, setMovies } = useState([])
+  
+  useEffect(() => {
+    let moviesList = [
+      {
+        id: 1,
+        title: "Star Wars",
+        release_date: "1977-05-25",
+        runtime: 121,
+        mpaa_rating: "PG",
+        description: "A long time ago, in a galaxy far, far away...",
+      },
+      {
+        id: 2,
+        title: "Highlander",
+        release_date: "1986-03-07",
+        runtime: 116,
+        mpaa_rating: "R",
+        description: "There can only be one...",
+      },
+      {
+        id: 3,
+        title: "Raiders of the Lost Ark",
+        release_date: "1981-06-12",
+        runtime: 115,
+        mpaa_rating: "PG-13",
+        description: "I'm making this up as I go...",
+      },
+    ]
+    setMovies(moviesList)
+  }, [])
 
   return (
     <div>
       <h2>Movies</h2>
-      <hr/>
+      <hr />
+      <table className='table table-striped table-hover'>
+        <thead>
+          <tr>
+            <th>Movie</th>
+            <th>Release Date</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map((m) => (
+            <tr key={m.id}>
+
+            </tr>
+            
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
