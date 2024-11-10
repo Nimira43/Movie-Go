437bad0647cc68af5374c0932f19ab9f1d7aa7a1
Nimira43 <n.rabone72@aol.com>
2024-11-10 05:59:12 +0000

    Tidying up CSS. Looks good.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
       <div className='text-center'>
         <h2 className='ember'>Search My Movie DB</h2>
         <hr />
-        <img src={Falcon} alt="Millenium Falcon"></img>
+        <img src={Falcon} alt="Millenium Falcon" className='mt-3'></img>
       </div>
     </>
   )
