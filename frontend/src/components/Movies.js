8213c7c6d7e413457cabba05518d869d782fdd34
Nimira43 <n.rabone72@aol.com>
2024-11-10 06:12:44 +0000

    Brought react-router down and set up Browser Router

---
+++ frontend/src/components/Movies.js
@@ -0,0 +1,12 @@
+const Movies = () => {
+  return(
+    <>
+      <div className='text-center'>
+        <h2 className='ember'>Movies</h2>
+        <hr />
+      </div>
+    </>
+  )
+}
+
+export default Movies
