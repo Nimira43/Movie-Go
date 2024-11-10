1b06e21cba1a7e99b5c535b7c52681ea7b69d5a3
Nimira43 <n.rabone72@aol.com>
2024-11-10 03:54:28 +0000

    Project rebooted. Created new template. Cleared out unwanted boilerplate. Brought in experimental CSS that was created in Project TWF

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className='container'>
+      <h1 className='logo'>My Movie DB</h1>
+
+      <button className='btn-wisdom-outline'>Submit</button>
+    </div>
+    
+  )
+}
+
+export default App
