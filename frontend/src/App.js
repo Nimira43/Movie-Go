696f5ce0a6482e6a91d4cd881bc5cd571b8a79c9
Nimira43 <n.rabone72@aol.com>
2024-11-10 04:46:02 +0000

    Setting text colour classes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 function App() {
   return (
     <div className='container'>
-      <div className="row">
-        <div className="col">
-          <h1 className='logo'>My Movie DB</h1>
+      <div className='row'>
+        <div className='col'>
+          <h1 className='logo mt-3'>My Movie DB</h1>
         </div>
-        <div className="col text-end">
-          <button className="btn-ember">Login</button>
+        <div className='col text-end'>
+          {/* <button className='btn-ember'>Login */}
+            <a href='#!' className='btn-ember'>Login</a> 
+          {/* </button> */}
+        </div>
+      </div>
+      <div className='row'>
+        <div className='col-md-2'>
+          <nav>
+            <div className="list-group">
+
+            </div>
+          </nav>
         </div>
       </div>
     </div>   
